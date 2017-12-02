943ce10939aa62b48bc4d23b9e7db436e9f34d9d
GoncaloRod <rodriguesgonalo604@gmail.com>
2017-12-02 12:56:35 +0000

    Got movement working

---
+++ Assets/Scrips/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+	private PlayerMotor motor;
+
+	public float speed = 5f;
+	public float lookSense = 3f;
+
+
+	private void Start ()
+	{
+		motor = GetComponent<PlayerMotor>();
+	}
+	
+	private void Update ()
+	{
+		// Calculate movement
+		float xMov = Input.GetAxis("Horizontal");
+		float yMov = Input.GetAxis("Vertical");
+		
+		Vector3 horizontalMovement = transform.right * xMov;
+		Vector3 verticalMovement = transform.forward * yMov;
+
+		// Final movement vector
+		Vector3 velocity = (horizontalMovement + verticalMovement) * speed;
+
+		// Apply movement
+		motor.Move(velocity);
+
+		// Calculate rotation
+		float yRot = Input.GetAxisRaw("Mouse X");
+		Vector3 rotation = new Vector3(0f, yRot, 0f) * lookSense;
+		motor.Rotate(rotation);
+	}
+}
