21ffb9c4143540e3e4e7fa7283dafb8bceef8b14
GoncaloRod <rodriguesgonalo604@gmail.com>
2017-12-02 10:20:14 +0000

    Spawning cars working

---
+++ Assets/CarSpawn.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class CarSpawn : MonoBehaviour
+{
+	public GameObject car;
+	public GameObject spawnPoint;
+	public float firstSpawnTime = 2f;
+	public float respawnSpeed = 5f;
+
+	void Start()
+	{
+		InvokeRepeating("SpawnCar", firstSpawnTime, respawnSpeed);
+	}
+
+	void SpawnCar()
+	{
+		Instantiate(car, spawnPoint.transform.position, spawnPoint.transform.rotation);
+	}
+}
