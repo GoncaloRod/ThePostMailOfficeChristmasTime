405da765bf5977d1ec83f0d1f452d0f9792a9d22
GoncaloRod <rodriguesgonalo604@gmail.com>
2017-12-03 10:32:01 +0000

    Mail spawn time increment working

--- Assets/Scrips/MailSpawner.cs
+++ Assets/Scrips/MailSpawner.cs
@@ -1,19 +1,42 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class MailSpawner : MonoBehaviour
 {
+	private float timer = 0;
+	public float respawnTime = 0f;
+	private PlayerStats stats;
+
+	public GameObject player;
 	public GameObject[] mails;
 	public GameObject spawnPoint;
-	public float firstSpawnTime = 5f;
-	public float respawnTime = 5f;
+	public float startingRespawnTime = 5f;
 	public float mailThrowForce = 200f;
 
-	private void Start ()
+	private void Start()
 	{
-		InvokeRepeating("SpawnMails", firstSpawnTime, respawnTime);
+		if (player != null)
+		{
+			stats = player.GetComponent<PlayerStats>();
+		}
+	}
+	
+	private void Update()
+	{
+		if (stats != null)
+		{
+			respawnTime = startingRespawnTime - ((float)stats.level / (float)10);
+		}
+
+		timer += Time.deltaTime;
+		if (timer >= respawnTime)
+		{
+			SpawnMail();
+			timer = 0;
+		}
 	}
 
-	private void SpawnMails()
+	private void SpawnMail()
 	{
 		GameObject obj = Instantiate(mails[Random.Range(0, mails.Length - 1)], spawnPoint.transform.position, spawnPoint.transform.rotation);
 		Rigidbody rb = obj.GetComponent<Rigidbody>();
--- Assets/Scrips/PlayerStats.cs
+++ Assets/Scrips/PlayerStats.cs
@@ -2,6 +2,13 @@
 
 public class PlayerStats : MonoBehaviour
 {
+	public int xpPerLevel = 100;
 	public float money = 5000f;
 	public long xp = 0;
+	public int level = 0;
+
+	private void Update()
+	{
+		level = (int)(xp / xpPerLevel);
+	}
 }
